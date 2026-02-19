828b69582ff2ad1a5a32f3ef27674cb7722cd3b3
tasnim-echo <tasnim@echologyx.com>
2026-02-19 17:00:24 +0600

    first page done

--- js/slider.js
+++ js/slider.js
@@ -1,1 +1,15 @@
-
+const swiper = new Swiper(".mySwiper", {
+  slidesPerView: 4, // how many slides visible
+  spaceBetween: 15, // space between slides
+  loop: true, // infinite loop
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    320: { slidesPerView: 1 },
+    480: { slidesPerView: 2 },
+    768: { slidesPerView: 3 },
+    1024: { slidesPerView: 4 },
+  },
+});
