b04841ee94a44eea3e20295df0ddc077ad4528ad
tasnim-echo <tasnim@echologyx.com>
2026-02-20 21:41:33 +0600

    view all button functionality done

--- js/view-all.js
+++ js/view-all.js
@@ -1,9 +1,16 @@
-const viewAll = document.querySelector(".view-all");
-const container = document.querySelector(".legacy-collection-container");
-viewAll.addEventListener("click", function () {
-  container.classList.toggle("show-all");
+const collections = document.querySelectorAll("section");
 
-  viewAll.textContent = container.classList.contains("show-all")
-    ? "View less"
-    : "View All";
+collections.forEach((section) => {
+  const viewAll = section.querySelector(".view-all");
+  const container = section.querySelector(".legacy-collection-images");
+
+  if (viewAll && container) {
+    viewAll.addEventListener("click", function () {
+      container.classList.toggle("show-all");
+
+      viewAll.textContent = container.classList.contain("show-all")
+        ? "View less"
+        : "View All";
+    });
+  }
 });
