81ff53cc421ade6e2ec5ad1a224ca1db5e2ce35a
tasnim-echo <tasnim@echologyx.com>
2026-02-24 20:23:33 +0600

    done with mobile nav functionality

---
+++ js/mobile-nav.js
@@ -0,0 +1,30 @@
+fetch("navbar.html")
+  .then((res) => res.text())
+  .then((data) => {
+    document.getElementById("navbar").innerHTML = data;
+
+    // INIT MOBILE MENU AFTER LOAD
+    initMobileMenu();
+  });
+
+function initMobileMenu() {
+  const menuIcon = document.querySelector(".hamburger");
+  const closeIcon = document.querySelector(
+    ".mobile-menu-container .close-icon",
+  );
+  const mobileMenuContainer = document.querySelector(".mobile-menu-container");
+
+  if (!menuIcon || !closeIcon || !mobileMenuContainer) {
+    console.log("Mobile nav elements not found");
+    return;
+  }
+
+  menuIcon.addEventListener("click", () => {
+    console.log("Hamburger clicked");
+    mobileMenuContainer.classList.add("active");
+  });
+
+  closeIcon.addEventListener("click", () => {
+    mobileMenuContainer.classList.remove("active");
+  });
+}
