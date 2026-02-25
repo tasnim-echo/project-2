bc79049f2dea73abe5c4bdcff387cab2eb673d2c
tasnim-echo <tasnim@echologyx.com>
2026-02-25 13:29:19 +0600

    added swiper scrollbar

--- js/slider.js
+++ js/slider.js
@@ -6,6 +6,12 @@ const swiper = new Swiper(".mySwiper", {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
+
+  scrollbar: {
+    el: ".swiper-scrollbar",
+    draggable: true,
+  },
+
   breakpoints: {
     320: { slidesPerView: 1 },
     480: { slidesPerView: 2 },
